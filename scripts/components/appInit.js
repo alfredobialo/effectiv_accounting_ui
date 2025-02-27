21faf3a9ce9a6a94c0a7a56309456fa670c56adb
Alfred Obialo <alfredcsdinc@gmail.com>
2025-02-27 18:25:22 +0100

    crypto Demo

--- scripts/components/appInit.js
+++ scripts/components/appInit.js
@@ -7,9 +7,30 @@
     
     function uiController(scope){
         scope.data = {
-            title : "New UI for Effectiv Business Solution",
-            author: "Alfred Obialo",
-            version : "2.5",
+            title : "Pi Network",
+            author: "Enewa Itodo",
+            version : "crypto",
+            dashboard : {
+                balance : 126.00,
+                miningBal : 0,
+            }
         };
+        function startFakeMining(durationInMillis){
+            var trackTime = 0; //  when = 60,000
+            window.setInterval(function(){
+               trackTime += durationInMillis;
+                scope.$apply(function(){
+                    scope.data.dashboard.miningBal += 0.045; 
+                    if(trackTime >= 60000){
+                        scope.data.dashboard.balance += scope.data.dashboard.miningBal;
+                        scope.data.dashboard.miningBal = 0.00;
+                        trackTime = 0;
+                    }
+                })
+                //cents
+            }, durationInMillis)
+        }
+        startFakeMining(3000);
+        
     }
 })(angular, window);
